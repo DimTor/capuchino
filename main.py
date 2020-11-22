0ed59447d2a5969809b5f086bc1300dabaa2dce6
DimTor <dima.torotenkov@yandex.ru>
2020-11-22 19:58:17 +0300

    5

---
+++ main.py
@@ -0,0 +1,70 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_3.clicked.connect(self.save_results)
+        self.pushButton_2.clicked.connect(self.new)
+        self.modified = {}
+        self.titles = None
+
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("SELECT * FROM coffee WHERE id=?",
+                         (int(self.lineEdit.text()),)).fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            pass
+            return
+        else:
+            self.tableWidget.setColumnCount(len(result[0]))
+            self.titles = [description[0] for description in cur.description]
+            # Заполнили таблицу полученными элементами
+            for i, elem in enumerate(result):
+                for j, val in enumerate(elem):
+                    self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+            self.modified = {}
+
+    def item_changed(self, item):
+        # Если значение в ячейке было изменено,
+        # то в словарь записывается пара: название поля, новое значение
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE coffee SET\n"
+            for key in self.modified.keys():
+                que += "{}='{}'\n".format(key, self.modified.get(key))
+            que += "WHERE id = ?"
+            cur.execute(que, (int(self.lineEdit.text()),))
+            self.con.commit()
+            self.modified.clear()
+
+    def new(self):
+        cur = self.con.cursor()
+        id = cur.execute("SELECT id FROM coffee").fetchall()
+        self.statusBar().showMessage(f'Вы создали новый кофе. Теперь вы можете изменить'
+                                     f' его загрузив id {max(id)[0] + 1}')
+        cur.execute("INSERT INTO coffee(id, name) VALUES(?, 'coffee_name')", (max(id)[0] + 1,))
+        self.con.commit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
